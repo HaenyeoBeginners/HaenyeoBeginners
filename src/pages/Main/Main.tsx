4fa823fe298f5e0f12d580e04403772eb13d78b5
confidential-nt <youyoy689@gmail.com>
2023-09-07 23:42:07 +0900

    [feat] 메인 화면 구현 완료

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+import styles from "./Main.module.css";
+import SlideCard from "../../components/SlideCard/SlideCard";
+import HashButton from "../../components/HashButton/HashButton";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import NewHaenyeoSwiper from "../../components/NewHaenyeoSwiper/NewHaenyeoSwiper";
+import ProfileCardSwiper from "../../components/ProfileCardSwiper/ProfileCardSwiper";
+import MapLanding from "../../components/MapLanding/MapLanding";
 
 export default function Main() {
-  return <div>여기는 메인 페이지</div>;
+  return (
+    <main className={styles.main}>
+      <h3 className={`h3 ${styles.title}`}>새롭게 영입했어요</h3>
+      <NewHaenyeoSwiper />
+      <ProfileCardSwiper />
+      <MapLanding />
+    </main>
+  );
 }
